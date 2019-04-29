c3553923aa0ddddf1a5b177e09254c48a1be70be
Mischa Spiegelmock <mischa@mvstg.biz>
2019-04-29 15:32:49 +0300

    Pipfile

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='Flask-Cognito',
-    version='1.10',
+    version='1.11',
     url='https://github.com/jetbridge/flask_cognito',
     license='ABRMS',
     author='Mischa Spiegelmock',
